9b7aa464c1fc53129459aee8c9284a8a4820e12d
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-19 15:28:43 +0000

    Added portfolio items

---
+++ app/assets/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+resources :portfolios
+  get 'pages/Home'
+
+  get 'pages/About'
+
+  get 'pages/Contact'
+
+  resources :blogs
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
