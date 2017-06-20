5bb2c3b781f21d9fe4198acf088f153ac04fc46e
Nebo Devcamp <nebograngerlomond@devcamp.com>
2017-06-20 16:31:40 +0000

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   resources :portfolios
-  get 'pages/Home'
-
-  get 'pages/About'
-
-  get 'pages/Contact'
+  
+  get 'About', to: 'pages#About'
+  get 'Contact', to: 'pages#Contact'
 
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root to: 'pages#Home'
 end
