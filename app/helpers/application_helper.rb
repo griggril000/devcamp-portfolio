b168ee2bd883c7289f5fcd6b03f3511166c05ad4
griggril000 <griggriley@gmail.com>
2017-07-08 20:30:21 +0000

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    RileyViewTool::Renderer.copyright 'Riley Grigg', 'All rights reserved'
+  end
 end
