09fdda23ad866b20f8d120a440e5cc6fd394ef51
griggril000 <griggriley@gmail.com>
2017-07-31 17:17:44 +0000

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -60,3 +60,15 @@ module ApplicationHelper
   end
 
 end
+
+def alerts
+  alert = (flash[:alert] || flash[:error] || flash[:notice])
+  
+  if alert
+    alert_generator alert
+  end
+end
+
+def alert_generator msg
+  js add_gritter(msg, title: "Riley Grigg Portfolio", sticky: false, time: 2500)
+end
