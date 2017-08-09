a91bcab1946ef8c67b1370a4ee5b93c91b85784a
griggril000 <griggriley@gmail.com>
2017-08-09 16:12:23 +0000

    added rack-cors gem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,4 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
-  config.font_assets.origin = 'https://riley-grigg-portfolio.herokuapp.com'
 end
