11ede5498cd1d10f6a0acf57771167829f2b7277
griggril000 <griggriley@gmail.com>
2017-08-17 03:39:37 +0000

    Hello

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,12 +11,12 @@
 // about supported directives.
 //
 //= require jquery
+//= require popper
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require html.sortable
 //= require tether
-//= require popper
 //= require cocoon
 //= require gritter
 //= require cable
